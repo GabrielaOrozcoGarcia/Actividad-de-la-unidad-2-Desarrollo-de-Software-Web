2d77d39ede49dfb538f3d00cbecd04acc39be1df
GabrielaOrozcoGarcia <gorozcog@unicartagena.com.co>
2026-05-22 17:53:16 -0500

    Correcciones

--- CRUD-USUARIOS-MVC/src/main/java/com/misiontic/repositorio/MascotaRepositorio.java
+++ CRUD-USUARIOS-MVC/src/main/java/com/misiontic/repositorio/MascotaRepositorio.java
@@ -2,10 +2,10 @@ package com.misiontic.repositorio;
 
 
 import com.misiontic.modelo.Mascota;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MascotaRepositorio extends JpaRepository<Mascota, Long> {
-    // Spring Data JPA genera el CRUD
+public interface MascotaRepositorio extends CrudRepository<Mascota, Long> {
+    // Spring Data genera el CRUD
 }
--- CRUD-USUARIOS-MVC/src/main/java/com/misiontic/repositorio/UsuarioRepositorio.java
+++ CRUD-USUARIOS-MVC/src/main/java/com/misiontic/repositorio/UsuarioRepositorio.java
@@ -1,15 +1,15 @@
 package com.misiontic.repositorio;
 
 import com.misiontic.modelo.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepositorio extends JpaRepository<Usuario, String> {
-    // Spring Data JPA genera el CRUD
+public interface UsuarioRepositorio extends CrudRepository<Usuario, String> {
+    // Spring Data genera el CRUD
     // La primary key es String (cedula)
-    // Buscar usuario por email para recuperación de contraseña
+    // Buscar usuario por email para recuperacion de contraseña
     Optional<Usuario> findByEmail(String email);
 }
